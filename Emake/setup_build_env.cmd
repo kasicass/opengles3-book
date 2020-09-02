5d0bd125ff3c5dda2f8444d456ba097b09fb5670
kasicass <kasicass@gmail.com>
2020-09-02 16:26:02 +0800

    ndk config

---
+++ Emake/setup_build_env.cmd
@@ -0,0 +1,4 @@
+set ANDROID_STUDIO_HOME="C:\Program Files\Android\Android Studio"
+set JAVA_HOME=%ANDROID_STUDIO_HOME%\jre
+set GRADLE_HOME=%ANDROID_STUDIO_HOME%\gradle\gradle-4.4
+set PATH=%PATH%;%JAVA_HOME%\bin;%GRADLE_HOME%\bin
