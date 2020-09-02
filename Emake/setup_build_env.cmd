ea26660635aaa6e43803284a4b656b67eb170802
kasicass <kasicass@gmail.com>
2020-09-02 19:21:04 +0800

    update

--- Emake/setup_build_env.cmd
+++ Emake/setup_build_env.cmd
@@ -1,5 +1,8 @@
+@echo off
+
 set ANDROID_STUDIO_HOME="C:\Program Files\Android\Android Studio"
 set JAVA_HOME=%ANDROID_STUDIO_HOME%\jre
 set PATH=%PATH%;%JAVA_HOME%\bin
 
 set ANDROID_SDK_ROOT="E:\AndroidSdk"
+set PATH=%PATH%;%ANDROID_SDK_ROOT%\platform-tools
